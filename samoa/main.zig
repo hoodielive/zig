69a0c2bc98a4f5e8e95a9a46f623184cf2ae7c07
hoodielive <hoodielive@gmail.com>
2021-12-28 01:41:58 -0500

    @as is explicit Type Coercion.

--- samoa/main.zig
+++ samoa/main.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 
 
 pub fn main() void {
-    const constant: i32 = 5;  // signed 32-bit constant
-    var variable: u32 = 5000; // unsigned 32-bit variable
+
+    // signed 32-bit constant
+    const constant: i32 = 5;  
+
+    // unsigned 32-bit variable
+    var variable: u32 = 5000; 
+
+    // @as forces Type Coercion.
+    const inferred_constant = @as(i32, 5);
+    var inferred_variable = @as(i32, 5000);
 }
