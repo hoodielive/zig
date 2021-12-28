aa88b6864ff04188b1393c2a396bff8b448a8075
hoodielive <hoodielive@gmail.com>
2021-12-28 02:00:50 -0500

    Zig doesn't play letting you write things and don't use them. How are we going to play with this language's concepts?

--- samoa/main.zig
+++ samoa/main.zig
@@ -4,12 +4,28 @@ const std = @import("std");
 pub fn main() void {
 
     // signed 32-bit constant
-    const constant: i32 = 5;  
+    const constant: i32 = 5;
 
     // unsigned 32-bit variable
-    var variable: u32 = 5000; 
+    var variable: u32 = 5000;
 
     // @as forces Type Coercion.
     const inferred_constant = @as(i32, 5);
     var inferred_variable = @as(i32, 5000);
+
+    // You always have to provide a value for
+    // constants and variables. If one is not
+    // available ~ use undefined.
+    const a: i32 = undefined;
+    var b: u32 = undefined;
+
+    // Arrays
+    const cArray = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
+    const dArray = [_]u8{ 'w', 'o', 'r', 'l', 'd' };
+
+    const length = dArray.len;
+    std.print(cArray);
+    std.debug.print(length);
+    std.debug.print(b);
+
 }
