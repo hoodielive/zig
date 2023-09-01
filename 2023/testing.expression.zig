146f78c4a3d789be20e63e39c648d4154f054e39
hoodielive <hoodielive@gmail.com>
2023-09-01 19:28:33 -0400

    Testing seems straightforward enough

--- 2023/testing.expression.zig
+++ 2023/testing.expression.zig
@@ -2,9 +2,33 @@ const expect = @import("std").testing.expect;
 const std = @import("std");
 const print = std.debug.print;
 
-pub fn main() !void {
+test "if statement" {
     const a = true;
     var x: u16 = 0;
     x += if (a) 1 else 2;
-    print(try expect(x == 1));
+    try expect(x == 1);
 }
+
+test "if boolean" {
+    const a: u32 = 5;
+    const b: u32 = 4;
+
+    if (a != b) {
+        try expect(true);
+    } else if (a == 9) {
+        unreachable;
+    } else {
+        unreachable;
+    }
+}
+
+test "if optional" {
+    const a: ?u32 = 0;
+    if (a) |value| {
+        try expect(value == 0);
+    } else {
+        unreachable;
+    }
+}
+
+pub fn main() !void {}
